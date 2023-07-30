d04dcb6eb75f3c236bd13ab65f4e17f0ecfb3540
Ahmedmamdouh40 <ahmedmamdouh2727@gmail.com>
2023-07-30 05:56:48 +0200

    get customer by id

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ def list_customers():
           respone = jsonify(str(e))
           respone.status_code = 400
           return respone
+     
+@app.route('/customer/get/<customer_id>' , methods=['GET'])
+def get_customer(customer_id):
+     try:
+          conn = mysql.connect()
+          cursor = conn.cursor(pymysql.cursors.DictCursor)
+          cursor.execute("SELECT id, name, phone_number, address FROM Customer WHERE id =%s", customer_id)
+          customer_obj = cursor.fetchone()
+          if not customer_obj:
+               return showMessage('Customer not found')
+          respone = jsonify(customer_obj)
+          respone.status_code = 200
+          cursor.close() 
+          conn.close() 
+          return respone
+     except Exception as e:
+          print(e)
